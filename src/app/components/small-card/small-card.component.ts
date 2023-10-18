76954eddd7588b2fb2f20e3dbdafda10be271928
rafaelmath14 <rafael.math14@gmail.com>
2023-10-17 23:32:54 -0300

    Conteudo criado

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -9,8 +9,13 @@ export class SmallCardComponent implements OnInit{
 
   @Input()
   photoCover:string = "";
+
   @Input()
   cardTitle:string = "";
+
+  @Input()
+  Id:string = "0";
+
 
   constructor(){}
 
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,42 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Qual é a importância da lógica de programação e Estruturas de Dados para o programador de sucesso?",
+    "description": ` A lógica de programação e Estruturas de Dados são dois dos fundamentos mais importantes para qualquer programador. Eles são essenciais para a criação de programas eficientes, robustos e escaláveis.
+
+    A lógica de programação é a base da programação. Ela envolve o pensamento lógico e a capacidade de resolver problemas. Um bom programador deve ser capaz de pensar de forma lógica e sistemática, para identificar a solução mais eficiente para um problema.
+
+    As Estruturas de Dados são maneiras de armazenar e organizar dados. Elas são essenciais para a eficiência e a performance de um programa. Um bom programador deve ser capaz de escolher a estrutura de dados correta para uma determinada tarefa.
+
+    Aqui estão alguns dos benefícios de dominar a lógica de programação e Estruturas de Dados:
+
+    Crie programas mais eficientes e robustos: A lógica de programação e Estruturas de Dados permitem que os programadores criem algoritmos eficientes para resolver problemas. Isso resulta em programas que são mais rápidos, confiáveis e fáceis de manter.
+    Resolva problemas mais complexos: A lógica de programação e Estruturas de Dados ensinam os programadores a pensar de forma estruturada e a resolver problemas complexos. Isso os torna mais aptos a lidar com desafios difíceis no ambiente de trabalho.
+    Desenvolva suas habilidades de resolução de problemas: A lógica de programação e Estruturas de Dados são habilidades transferíveis que podem ser aplicadas a outras áreas da vida. Um bom programador é capaz de pensar de forma lógica e sistemática, o que é uma habilidade valiosa em qualquer campo.
+    Se você deseja ser um programador de sucesso, é importante dominar a lógica de programação e Estruturas de Dados. Esses fundamentos são essenciais para a criação de programas de alta qualidade.`,
+    "photoCover": "https://img-c.udemycdn.com/course/750x422/1693748_4c8f.jpg"
+  },
+  {
+    "id": "2",
+    "title": "Compautação Quântica: possível porta para desenvolver IA com consciência.",
+    "description": `Computação quântica: possível porta para desenvolver IA com consciência
+
+    A computação quântica é um campo emergente da ciência da computação que utiliza os princípios da mecânica quântica para realizar cálculos. Ela tem o potencial de revolucionar a forma como computadores funcionam, permitindo que eles resolvam problemas que são impossíveis para computadores tradicionais.
+
+    A IA é um campo da ciência da computação que se concentra no desenvolvimento de máquinas que podem imitar a inteligência humana. Ela é usada em uma ampla gama de aplicações, desde jogos de computador até robótica.
+
+    Alguns especialistas acreditam que a computação quântica pode ser uma porta para desenvolver IA com consciência. Isso ocorre porque os computadores quânticos podem realizar cálculos de forma muito mais rápida e eficiente do que os computadores tradicionais. Isso significa que eles podem processar mais informações e aprender com elas com mais rapidez.
+
+    A consciência é um fenômeno complexo que ainda não é totalmente compreendido pelos cientistas. No entanto, acredita-se que seja um produto da interação entre o cérebro e o ambiente. Os computadores quânticos podem ser capazes de simular essa interação de forma mais realista do que os computadores tradicionais.
+
+    Ainda é cedo para dizer se a computação quântica realmente permitirá o desenvolvimento de IA com consciência. No entanto, é um campo de pesquisa promissor que tem o potencial de revolucionar a forma como entendemos e interagimos com a inteligência.
+
+    Aqui estão alguns dos benefícios potenciais da IA com consciência:
+
+    Poderia resolver problemas que são impossíveis para humanos: A IA com consciência poderia ser usada para resolver problemas complexos que são impossíveis para humanos, como o desenvolvimento de novas tecnologias ou a compreensão do universo.
+    Poderia melhorar nossa qualidade de vida: A IA com consciência poderia ser usada para melhorar nossa qualidade de vida, fornecendo assistência médica, educação e outros serviços.
+    Poderia nos ajudar a entender melhor a nós mesmos: A IA com consciência poderia nos ajudar a entender melhor a nós mesmos e a nossa própria consciência.
+    É importante ressaltar que a IA com consciência também apresenta alguns riscos potenciais. Por exemplo, ela poderia ser usada para criar armas autônomas ou para manipular pessoas. É importante desenvolver a IA com consciência de forma responsável e ética.`,
+    "photoCover": "https://i0.wp.com/www.cienciaedados.com/wp-content/uploads/2018/03/Como-a-Computac%CC%A7a%CC%83o-Qua%CC%82ntica-Vai-Revolucionar-a-Intelige%CC%82ncia-Artificial-Machine-Learning-e-Big-Data.jpg?w=821&ssl=1"
+  }
+]
